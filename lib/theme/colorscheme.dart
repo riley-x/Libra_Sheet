478cf5add331283f204d692a10852550ab173256
Riley Xu <riley.xu@gmail.com>
2023-11-13 01:04:25 -0500

    add syncfusion chart

---
+++ lib/theme/colorscheme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final libraDarkColorScheme = ColorScheme.fromSeed(seedColor: Colors.lightBlue);
+
+// ColorScheme(
+//   brightness: Brightness.dark, 
+//   primary: primary, 
+//   onPrimary: onPrimary, 
+//   secondary: secondary, 
+//   onSecondary: onSecondary, 
+//   error: error, 
+//   onError: onError, 
+//   background: background, 
+//   onBackground: onBackground, 
+//   surface: surface, 
+//   onSurface: onSurface,
+// )
