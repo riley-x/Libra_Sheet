38342324e378cf48cf650a73b6c42206fc9905d1
Riley Xu <riley.xu@gmail.com>
2023-11-30 19:04:08 -0500

    documentation

--- lib/data/app_state/libra_app_state.dart
+++ lib/data/app_state/libra_app_state.dart
@@ -61,19 +61,6 @@ class LibraAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  final List<TimeValue> chartData = [
-    TimeValue.monthStart(2019, 1, 35),
-    TimeValue.monthStart(2019, 2, 28),
-    TimeValue.monthStart(2019, 3, 34),
-    TimeValue.monthStart(2019, 4, 32),
-    TimeValue.monthStart(2019, 5, 40),
-    TimeValue.monthStart(2019, 6, 35),
-    TimeValue.monthStart(2019, 7, 28),
-    TimeValue.monthStart(2019, 8, 34.140001),
-    TimeValue.monthStart(2019, 9, 32.01),
-    TimeValue.monthStart(2019, 10, 40.10)
-  ];
-
   //--------------------------------------------------------------------------------
   // Accounts
   //--------------------------------------------------------------------------------
--- lib/data/app_state/transaction_service.dart
+++ lib/data/app_state/transaction_service.dart
@@ -5,23 +5,17 @@ import 'package:libra_sheet/data/database/transactions.dart';
 import 'package:libra_sheet/data/objects/transaction.dart';
 import 'package:libra_sheet/tabs/transaction/transaction_filter_state.dart';
 
-// TODO is this class even necessary? Just update listeners for the appState?
-
 /// Helper class for managing transactions. Every widget that monitors transactions should probably
-/// watch this service, so that they can be notified when transactions are added or edited (which,
-/// because of reimbursements, can affect all other transactions).
+/// watch this service, so that they can be notified when transactions are added or edited (which
+/// affects all value-based entities and, because of reimbursements, can affect all other
+/// transactions).
+///
+/// This service isn't meant to be used in the widget tree, although it is provided nonetheless to
+/// enable easy callbacks, usually via context.read().
 class TransactionService extends ChangeNotifier {
-  //----------------------------------------------------------------------------
-  // Fields
-  //----------------------------------------------------------------------------
   final LibraAppState appState;
   TransactionService(this.appState);
 
-  // final Map<Object, List<Transaction>> lists = {};
-
-  //----------------------------------------------------------------------------
-  // Database Interface
-  //----------------------------------------------------------------------------
   Future<List<Transaction>> load(TransactionFilters filters) async {
     final ts = await loadTransactions(
       filters,
