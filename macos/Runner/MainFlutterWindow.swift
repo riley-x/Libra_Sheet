5927f5932187a70b847b732cfe70f5db90015b59
Riley Xu <riley.xu@gmail.com>
2023-12-15 13:47:08 -0500

    change app name

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,7 +5,7 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     self.contentViewController = flutterViewController
-    self.setFrame(NSRect(x:0, y:0, width: 1440, height: 900), display: true)
+    self.setFrame(NSRect(x:10, y:10, width: 1440, height: 900), display: true)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
