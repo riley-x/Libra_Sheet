3a123c67f29c33993ebb5a9b83839e1d901c4ac2
Riley Xu <riley.xu@gmail.com>
2025-01-31 00:44:12 -0500

    fix spacing

--- lib/graphing/cartesian/income_expense_tooltip.dart
+++ lib/graphing/cartesian/income_expense_tooltip.dart
@@ -101,7 +101,7 @@ class IncomeExpenseTooltip extends StatelessWidget {
       }
 
       /// Spacing
-      if (incomeLabels.length > 1 || expenseLabels.length > 1) {
+      if (incomeLabels.length > 1 && expenseLabels.length > 1) {
         entries.add(const SizedBox(height: 12));
       }
 
