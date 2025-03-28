4c353cd3ae69e64059d6a8fee72f73b557e139d5
Riley Xu <riley.xu@gmail.com>
2025-03-28 01:06:28 -0400

    add account checkbox grouping

--- lib/components/menus/account_checkbox_menu.dart
+++ lib/components/menus/account_checkbox_menu.dart
@@ -18,37 +18,58 @@ class AccountChips extends StatelessWidget {
 
   final TextStyle? style;
   final Set<Account> selected;
-  final Function(Account, bool?)? onChanged;
+  final Function(AccountOrHeader, bool?)? onChanged;
 
   /// Can update [selected] in-place using default behavior. This callback must be set to be
   /// notified of the change, and [onChaged] must be null.
-  final Function(Account, bool?)? whenChanged;
-
-  void defaultOnChanged(Account account, bool? val) {
-    if (onChanged != null) {
-      onChanged!(account, val);
-      return;
-    }
-    if (whenChanged == null) return;
-    if (val == true) {
-      selected.add(account);
-    } else {
-      selected.remove(account);
-    }
-    whenChanged!.call(account, val);
-  }
+  final Function(AccountOrHeader, bool?)? whenChanged;
 
   @override
   Widget build(BuildContext context) {
     assert(onChanged == null || whenChanged == null);
     final accounts = context.watch<AccountState>().list;
+
+    bool isChecked(AccountOrHeader account) {
+      if (account.account != null) {
+        return selected.contains(account.account);
+      } else {
+        return accounts.every((acc) => acc.type != account.header || selected.contains(acc));
+      }
+    }
+
+    void defaultOnChanged(AccountOrHeader account, bool? val) {
+      if (onChanged != null) {
+        onChanged!(account, val);
+        return;
+      }
+      if (whenChanged == null) return;
+
+      if (account.header != null) {
+        if (isChecked(account)) {
+          selected.removeWhere((acc) => acc.type == account.header);
+        } else {
+          for (final acc in accounts) {
+            if (acc.type == account.header) selected.add(acc);
+          }
+        }
+      } else {
+        if (val == true) {
+          selected.add(account.account!);
+        } else {
+          selected.remove(account.account!);
+        }
+      }
+
+      whenChanged!.call(account, val);
+    }
+
     return Column(
       children: [
         TitleRow(
           title: Text("Accounts", style: style ?? Theme.of(context).textTheme.titleMedium),
           right: AccountCheckboxMenu(
             accounts: accounts,
-            isChecked: selected.contains,
+            isChecked: isChecked,
             onChanged: defaultOnChanged,
           ),
         ),
@@ -73,7 +94,7 @@ class AccountChips extends StatelessWidget {
                 LibraChip(
                   acc.name,
                   color: acc.color,
-                  onTap: () => defaultOnChanged(acc, false),
+                  onTap: () => defaultOnChanged(AccountOrHeader.account(acc), false),
                 ),
             ],
           ),
@@ -91,17 +112,68 @@ class AccountCheckboxMenu extends StatelessWidget {
   });
 
   final List<Account> accounts;
-  final bool? Function(Account acc)? isChecked;
-  final Function(Account acc, bool? val)? onChanged;
+  final bool? Function(AccountOrHeader acc)? isChecked;
+  final Function(AccountOrHeader acc, bool? val)? onChanged;
+
+  static const groupingMinTotal = 9;
+  static const groupingMinPerGroup = 3;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownCheckboxMenu<Account>(
+    final cashAccounts = <Account>[];
+    final bankAccounts = <Account>[];
+    final investmentAccounts = <Account>[];
+    final liabilityAccounts = <Account>[];
+
+    var doGrouping = false;
+    if (accounts.length >= groupingMinTotal) {
+      for (final account in accounts) {
+        final accountList = switch (account.type) {
+          AccountType.cash => cashAccounts,
+          AccountType.bank => bankAccounts,
+          AccountType.investment => investmentAccounts,
+          AccountType.liability => liabilityAccounts,
+        };
+        accountList.add(account);
+      }
+
+      final listsWithMin = (cashAccounts.length >= groupingMinPerGroup ? 1 : 0) +
+          (bankAccounts.length >= groupingMinPerGroup ? 1 : 0) +
+          (investmentAccounts.length >= groupingMinPerGroup ? 1 : 0) +
+          (liabilityAccounts.length >= groupingMinPerGroup ? 1 : 0);
+      doGrouping = listsWithMin >= 2;
+    }
+
+    final items = doGrouping
+        ? [
+            if (cashAccounts.isNotEmpty) AccountOrHeader.header(AccountType.cash),
+            for (final account in cashAccounts) AccountOrHeader.account(account),
+            if (bankAccounts.isNotEmpty) AccountOrHeader.header(AccountType.bank),
+            for (final account in bankAccounts) AccountOrHeader.account(account),
+            if (investmentAccounts.isNotEmpty) AccountOrHeader.header(AccountType.investment),
+            for (final account in investmentAccounts) AccountOrHeader.account(account),
+            if (liabilityAccounts.isNotEmpty) AccountOrHeader.header(AccountType.liability),
+            for (final account in liabilityAccounts) AccountOrHeader.account(account),
+          ]
+        : [
+            for (final account in accounts) AccountOrHeader.account(account),
+          ];
+
+    return DropdownCheckboxMenu<AccountOrHeader>(
       icon: Icons.add,
-      items: accounts,
-      builder: accountMenuBuilder,
+      items: items,
+      builder: (context, account) =>
+          accountOrHeaderMenuBuilder(context, account, containsHeaders: doGrouping),
       isChecked: isChecked,
       onChanged: onChanged,
     );
   }
+}
+
+class AccountOrHeader {
+  final Account? account;
+  final AccountType? header;
+
+  AccountOrHeader.account(this.account) : header = null;
+  AccountOrHeader.header(this.header) : account = null;
 }
--- lib/components/menus/account_menu_builder.dart
+++ lib/components/menus/account_menu_builder.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:libra_sheet/components/menus/account_checkbox_menu.dart';
 import 'package:libra_sheet/data/objects/account.dart';
+
+Widget accountOrHeaderMenuBuilder(
+  BuildContext context,
+  AccountOrHeader? account, {
+  bool containsHeaders = false,
+  String? nullText,
+}) {
+  if (account?.header != null) {
+    return LimitedBox(
+      maxWidth: 400,
+      child: Text(
+        "${account!.header!.label} Accounts",
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        style: Theme.of(context).textTheme.labelLarge,
+      ),
+    );
+  } else {
+    return accountMenuBuilder(
+      context,
+      account?.account,
+      indent: containsHeaders,
+      nullText: nullText,
+    );
+  }
+}
 
 /// This callback is used to draw a single account row in dropdown menus.
 Widget accountMenuBuilder(
   BuildContext context,
   Account? account, {
+  bool indent = false,
   String? nullText,
 }) {
   final text = Text(
@@ -19,6 +47,7 @@ Widget accountMenuBuilder(
     maxWidth: 400, // prevent errors from [Flexible] below
     child: Row(
       children: [
+        if (indent) const SizedBox(width: 8),
         Container(
           width: 4,
           height: 30,
